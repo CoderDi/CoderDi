c219a427c1ab89aac960ce67ad4ff24ae2af2e54
Dmitry <lvbnhbq2103@mail.ru>
2017-06-18 21:57:53 +0700

    First day
    Header, portfolio, contacts

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,51 @@
     Custom
  */
 //= partials/helper.js
+
+$(document).ready(function() {
+  
+  $("#back-top").hide();
+    $(function () {
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+          $('#back-top').fadeIn();
+        } else {
+          $('#back-top').fadeOut();
+        }
+      });
+      $('#back-top a').click(function () {
+        $('body,html').animate({scrollTop: 0}, 600);
+        return false;
+      });
+    });
+  
+	$('.menu-blind').click(function() {
+    $('.menu-blind__block').toggleClass('menu-blind__block-visible');
+		$(this).toggleClass('menu-blind-visible');
+		$('body').toggleClass('body-hidden');
+	});
+  
+  $('.header__titles').click(function() {
+    $('.header__titles').toggleClass('visible');
+  });
+  
+  $('.portfolio__description_switch-comp').click(function() {
+    $(this).parents('.portfolio__item').find('.portfolio__item_slide').removeClass('portfolio-visible');
+    $(this).parents('.portfolio__item').find('.portfolio__item_slide1').addClass('portfolio-visible');
+  });
+  
+  $('.portfolio__description_switch-ipad').click(function() {
+    $(this).parents('.portfolio__item').find('.portfolio__item_slide').removeClass('portfolio-visible');
+    $(this).parents('.portfolio__item').find('.portfolio__item_slide2').addClass('portfolio-visible');
+  });
+  
+  $('.portfolio__description_switch-iphone').click(function() {
+    $(this).parents('.portfolio__item').find('.portfolio__item_slide').removeClass('portfolio-visible');
+    $(this).parents('.portfolio__item').find('.portfolio__item_slide3').addClass('portfolio-visible');
+  });
+  
+	
+  
+});
+
+
