a7bae8de30b895ad8b5d2e301584d29b1ee2ea8d
CoderDi <lvbnhbq2103@mail.ru>
2020-07-12 23:29:06 +0700

    commit

--- js/main.js
+++ js/main.js
@@ -1,44 +1,16 @@
 $(document).ready(function () {
-  $(".butter").click(function(){
-    $(this).toggleClass("butter-hover");
-    $(".menu").toggleClass("menu-open");
+  $(".js-watch-item").click(function(){
+    $("html").addClass("no-scroll");
+    $("#header").addClass("no-scroll");
+    $(".js-popup-title").text($(this).parents(".item").find(".item__title").text());
+    $(".popup").addClass("active");
+  });
+  $(".js-close").click(function(){
+    $("html").removeClass("no-scroll");
+    $("#header").removeClass("no-scroll");
+    $(".popup").removeClass("active");
   });
 
-  $(".slide-arrow").click(function(){
-    $.fn.fullpage.moveSectionDown();
-  });
-
-  $(".screen__btn").click(function(){
-    $.fn.fullpage.setAllowScrolling(false);
-    $.fn.fullpage.setKeyboardScrolling(false);
-    var then = $(this).parents(".screen-part");
-    $(then).css("width","100%");
-    $(this).parents(".screen").find(".screen-left").css("left","-50%");
-    $(".slide-arrow").addClass("slide-arrow--hide");
-    setTimeout(function(){
-      $(then).addClass("screen-open");
-      $(then).find(".screen-content").addClass("screen-content--hide");
-    }, 500);
-  });
-
-  $(".screen-part__close").click(function(){
-    $.fn.fullpage.setAllowScrolling(true);
-    $.fn.fullpage.setKeyboardScrolling(true);
-    var then = $(this).parents(".screen-part");
-    $(then).removeClass("screen-open");
-    setTimeout(function(){
-      $(then).css("width","50%");
-      $(then).parents(".screen").find(".screen-left").css("left","0");
-      $(then).find(".screen-content").removeClass("screen-content--hide");
-      $(".slide-arrow").removeClass("slide-arrow--hide");
-    }, 500);
-    
-  });
-
-
-  $('#fullpage').fullpage({
-    
-  });
 
 
   $("a[href^='#']").click(function(){
